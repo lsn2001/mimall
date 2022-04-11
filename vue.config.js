b01469a44c64829a8de76e31d568cc3ccfaddf46
lsn2001 <642634743@qq.com>
2022-04-11 21:43:24 +0800

    day1

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8081,
+    proxy: {
+      '/activity': {
+        target: 'https://www.imooc.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '/activity':'/activity'
+        }
+      }
+    }
+  }
+}
